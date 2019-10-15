f39599a21ba95f5bb79328cea2be2d5041d12046
João Dias <joao.dias@feedzai.com>
2019-10-16 00:45:15 +0100

    Updated eslint rules.

--- eslint/rules/general.js
+++ eslint/rules/general.js
@@ -6,19 +6,19 @@
  */
 module.exports = {
 	rules: {
-		'jsx-a11y/href-no-hash': 'off',
-		'jsx-a11y/anchor-is-valid': [
-			'warn',
+		"jsx-a11y/href-no-hash": "off",
+		"jsx-a11y/anchor-is-valid": [
+			"warn",
 			{
-				aspects: ['invalidHref'],
+				aspects: ["invalidHref"],
 			},
 		],
-		'jsx-a11y/label-has-for': 0,
+		"jsx-a11y/label-has-for": 0,
 	},
 };
 
 module.exports = {
-	plugins: ['import'],
+	plugins: ["import"],
 
 	env: {
 		es6: true,
@@ -37,28 +37,28 @@ module.exports = {
 	},
 
 	rules: {
-		'import/no-unresolved': 'error',
-		'import/no-extraneous-dependencies': [
-			'error',
+		"import/no-unresolved": "error",
+		"import/no-extraneous-dependencies": [
+			"error",
 			{
 				devDependencies: [
-					'**/*.test.js',
-					'**/*.spec.js',
-					'**/*.stories.tsx',
-					'**/*.stories.jsx',
+					"**/*.test.js",
+					"**/*.spec.js",
+					"**/*.stories.tsx",
+					"**/*.stories.jsx",
 				],
 			},
 		],
-		'comma-dangle': [1, 'always-multiline'],
+		"comma-dangle": [1, "always-multiline"],
 
-		'no-param-reassign': [
-			'error',
-			{ ignorePropertyModificationsFor: ['draftState', 'draft'] },
+		"no-param-reassign": [
+			"error",
+			{ ignorePropertyModificationsFor: ["draftState", "draft"] },
 		],
-		'class-methods-use-this': 'off',
-		'require-jsdoc': 2,
-		'lines-around-comment': [
-			'error',
+		"class-methods-use-this": "off",
+		"require-jsdoc": 2,
+		"lines-around-comment": [
+			"error",
 			{
 				beforeBlockComment: true,
 				afterBlockComment: false,
@@ -72,16 +72,16 @@ module.exports = {
 				allowArrayEnd: true,
 			},
 		],
-		quotes: ['warn', 'single'],
-		indent: 'off', // Disallow modifying variables that are declared using const
-		'no-const-assign': 'error',
+		quotes: ["warn", "double"],
+		indent: "off", // Disallow modifying variables that are declared using const
+		"no-const-assign": "error",
 
 		// /!\ If a variable is never reassigned, using the const declaration is better.
-		'prefer-const': 'warn',
+		"prefer-const": "warn",
 
 		// Require space before/after arrow function’s arrow
-		'arrow-spacing': [
-			'error',
+		"arrow-spacing": [
+			"error",
 			{
 				before: true,
 				after: true,
@@ -89,28 +89,28 @@ module.exports = {
 		],
 
 		// Verify calls of super() in constructors
-		'constructor-super': 'error',
+		"constructor-super": "error",
 
 		// Disallow arrow functions where they could be confused with comparisons
-		'no-confusing-arrow': [
-			'error',
+		"no-confusing-arrow": [
+			"error",
 			{
 				allowParens: true,
 			},
 		],
 
 		// Disallow duplicate name in class members
-		'no-dupe-class-members': 'error',
+		"no-dupe-class-members": "error",
 
 		// Disallow use of this/super before calling super() in constructors.
-		'no-this-before-super': 'error',
+		"no-this-before-super": "error",
 
 		// Disallow unnecessary constructor
-		'no-useless-constructor': 'error',
+		"no-useless-constructor": "error",
 
 		// Disallow renaming import, export, and destructured assignments to the same name
-		'no-useless-rename': [
-			'error',
+		"no-useless-rename": [
+			"error",
 			{
 				ignoreDestructuring: false,
 				ignoreImport: false,
@@ -119,17 +119,17 @@ module.exports = {
 		],
 
 		// Suggest using template literals instead of string concatenation.
-		'prefer-template': 'error',
+		"prefer-template": "error",
 
 		// Enforce Usage of Spacing in Template Strings
-		'template-curly-spacing': 'error',
+		"template-curly-spacing": "error",
 
 		// /!\ Disallow template literal placeholder syntax in regular strings
-		'no-template-curly-in-string': 'warn',
+		"no-template-curly-in-string": "warn",
 
 		// disallow importing from the same path more than once
 		// https://eslint.org/docs/rules/no-duplicate-imports
 		// replaced by https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
-		'no-duplicate-imports': 'off',
+		"no-duplicate-imports": "off",
 	},
 };
--- eslint/rules/typescript.js
+++ eslint/rules/typescript.js
@@ -5,28 +5,29 @@
  * @since 1.0.0
  */
 module.exports = {
-	parser: '@typescript-eslint/parser',
+	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		jsx: true,
 		useJSXTextNode: true,
 	},
-	extends: ['plugin:@typescript-eslint/recommended'],
-	plugins: ['@typescript-eslint'],
+	extends: ["plugin:@typescript-eslint/recommended"],
+	plugins: ["@typescript-eslint"],
 	rules: {
-		'@typescript-eslint/interface-name-prefix': [2, 'always'],
-		'@typescript-eslint/class-name-casing': 2,
-		'@typescript-eslint/explicit-member-accessibility': 0,
-		'@typescript-eslint/indent': ['error', 'tab'],
-		'@typescript-eslint/no-explicit-any': 0,
-		'@typescript-eslint/explicit-function-return-type': 0,
+		"@typescript-eslint/interface-name-prefix": [2, "always"],
+		"@typescript-eslint/class-name-casing": 2,
+		"@typescript-eslint/explicit-member-accessibility": 0,
+		"@typescript-eslint/indent": ["error", "tab"],
+		"@typescript-eslint/no-explicit-any": 0,
+		"@typescript-eslint/explicit-function-return-type": 0,
+		"@typescript-eslint/no-var-requires": 0,
 	},
 	settings: {
-		'import/parsers': {
-			'@typescript-eslint/parser': ['.ts', '.tsx'],
+		"import/parsers": {
+			"@typescript-eslint/parser": [".ts", ".tsx"],
 		},
-		'import/resolver': {
+		"import/resolver": {
 			typescript: {
-				directory: './',
+				directory: "./",
 			},
 		},
 	},
