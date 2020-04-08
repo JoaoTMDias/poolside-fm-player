6685c618be6f4cb858cf4cd53f01703024b63d5d
João Dias <joao.dias@feedzai.com>
2020-04-08 02:29:51 +0100

    Added more cypress tests

---
+++ cypress/integration/change-playlists.js
@@ -0,0 +1,22 @@
+// / <reference types="cypress" />
+import { WINDOW_WIDTH, WINDOW_HEIGHT } from "../../src/data/constants/native";
+
+describe("Change Playlists", () => {
+	beforeEach(() => {
+		cy.visit("http://localhost:3000");
+		cy.viewport(WINDOW_WIDTH, WINDOW_HEIGHT);
+	});
+
+	it("should change the playlist to Indie Summer", () => {
+		cy.get("[data-testid='component-select-button']").click();
+		cy.get(".select-input__option[aria-selected='true'] button")
+			.focus()
+			.type("{downarrow}");
+		cy.get("#indie-summer-id-1 button")
+			.should("have.focus")
+			.type("{enter}")
+			.trigger("click");
+		cy.get("[data-testid='component-select-button'] #channel-title")
+			.contains("Indie Summer");
+	});
+});
